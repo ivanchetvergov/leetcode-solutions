0fce0c906b0ef0133c7b0d2b8b057855bf5de5fd
ivanchetvergov <ivar5165885@gmail.com>
2025-10-13 23:32:31 +0300

    add: 47  problem  greedy solve

---
+++ array/53. Maximum Subarray.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        ! классический жадный алгос
+        идея в том чтобы следить за локальным
+        и глобальным максимум
+        и резать локальный если он меньше нового эл
+        """
+        N = len(nums)
+        if N == 0:
+            return 0
+        
+        glob_max = nums[0]
+        local_max = nums[0]
+        
+        for i in range(1, N):
+            cur_num = nums[i]
+            
+            # определяем локальный максимум
+            local_max = max(cur_num, local_max + cur_num)
+            # глобальный максимум
+            glob_max = max(glob_max, local_max)
+            
+        return glob_max
